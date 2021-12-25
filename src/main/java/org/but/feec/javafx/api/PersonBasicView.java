bde9a0a1001f4ac14468126c9d976e8afb87e0c2
not4pedro <peto.leitmann@gmail.com>
2021-12-26 00:09:43 +0100

    Initial commit

---
+++ src/main/java/org/but/feec/javafx/api/PersonBasicView.java
@@ -0,0 +1,75 @@
+package org.but.feec.javafx.api;
+
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class PersonBasicView {
+    private LongProperty id = new SimpleLongProperty();
+    private StringProperty city = new SimpleStringProperty();
+    private StringProperty email = new SimpleStringProperty();
+    private StringProperty givenName = new SimpleStringProperty();
+    private StringProperty familyName = new SimpleStringProperty();
+
+    public Long getId() {
+        return idProperty().get();
+    }
+
+    public void setId(Long id) {
+        this.idProperty().setValue(id);
+    }
+
+    public String getCity() {
+        return cityProperty().get();
+    }
+
+    public void setCity(String city) {
+        this.cityProperty().setValue(city);
+    }
+
+    public String getEmail() {
+        return emailProperty().get();
+    }
+
+    public void setEmail(String email) {
+        this.emailProperty().setValue(email);
+    }
+
+    public String getGivenName() {
+        return givenNameProperty().get();
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenNameProperty().setValue(givenName);
+    }
+
+    public String getFamilyName() {
+        return familyNameProperty().get();
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyNameProperty().setValue(familyName);
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public StringProperty cityProperty() {
+        return city;
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    public StringProperty givenNameProperty() {
+        return givenName;
+    }
+
+    public StringProperty familyNameProperty() {
+        return familyName;
+    }
+
+}
